ff92a7bfdb903a5b468a88b0e3a2710b276a9d3c
maxxiyboiy <98927442+maxxiyboiy@users.noreply.github.com>
2024-01-24 20:37:41 -0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+    printf("Hello world, this is Max Kim.\n");
+    printf("I am in section 1001.\n\n")
+
+    exit(0);
+}
